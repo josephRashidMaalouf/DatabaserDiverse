f4a3aa4a40990e27927da083e129fe8d180ef3a2
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2023-11-20 16:05:28 +0100

    Update Labb1.sql
    Begun populating tables

--- Labb1/Labb1.sql
+++ Labb1/Labb1.sql
@@ -2,6 +2,8 @@
 
 USE Labb1_Bokhandel;
 
+--###Create tables###---
+
 --CREATE TABLE Authors
 --	(
 --		Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
@@ -91,12 +93,49 @@ USE Labb1_Bokhandel;
 --		CONSTRAINT [FK_IsbnId_Isbn13] FOREIGN KEY ([Isbn13]) REFERENCES Books(Isbn13)
 --	);
 
-CREATE TABLE IventoryBalance
-(
-	[StoreId] INT FOREIGN KEY REFERENCES Stores(Id) NOT NULL,
-	[Isbn13] NVARCHAR(13) CHECK((Isbn13 NOT LIKE '%[^0-9]%') AND LEN(Isbn13) = 13) NOT NULL,
-	[Quantity] INT NOT NULL,
-	CONSTRAINT [PK_Store_Book] PRIMARY KEY ([StoreId], [Isbn13]),
-	CONSTRAINT [FK_StoreId_Stores] FOREIGN KEY ([StoreId]) REFERENCES Stores(Id),
-	CONSTRAINT [FK_Isbn13_Books2] FOREIGN KEY ([Isbn13]) REFERENCES Books(Isbn13)
-);
+--CREATE TABLE IventoryBalance
+--(
+--	[StoreId] INT FOREIGN KEY REFERENCES Stores(Id) NOT NULL,
+--	[Isbn13] NVARCHAR(13) CHECK((Isbn13 NOT LIKE '%[^0-9]%') AND LEN(Isbn13) = 13) NOT NULL,
+--	[Quantity] INT NOT NULL,
+--	CONSTRAINT [PK_Store_Book] PRIMARY KEY ([StoreId], [Isbn13]),
+--	CONSTRAINT [FK_StoreId_Stores] FOREIGN KEY ([StoreId]) REFERENCES Stores(Id),
+--	CONSTRAINT [FK_Isbn13_Books2] FOREIGN KEY ([Isbn13]) REFERENCES Books(Isbn13)
+--);
+
+--###Populate the tables###---
+
+--INSERT INTO Publishers
+--VALUES
+--	('Studentlitteratur', 'Lund'),
+--	('AstridLindgrenTextAB', 'Stockholm'),
+--	('Raben & Sjögren', 'Stockholm'),
+--	('Nordstedts', 'Stockholm'),
+--	('HarperCollinsUK', 'Manchester'),
+--	('Harper Perennial', 'New York');
+--INSERT INTO Publishers VALUES ('Albert Bonniers', 'Stockholm');
+
+--INSERT INTO Authors
+--VALUES
+--	('Zhiwar', 'Rashid', '1998-09-16'),
+--	('Astrid', 'Lindgren', '1907-11-14'),
+--	('Jonas Hassen', 'Khemiri', '1978-12-27'),
+--	('George R.R', 'Martin', '1948-12-27'),
+--	('Yuval Noah', 'Harari', '1976-02-24'),
+--	('Bo', 'Sandberg', '1952-06-29'),
+--	('Sven', 'Faugert', '1945-10-12');
+
+
+--INSERT INTO Publisher_Author (AuthorId, PublisherId) 
+--VALUES
+--	(1,15),
+--	(2,13),
+--	(2,14),
+--	(3,18),
+--	(3,15),
+--	(4,16),
+--	(5,17),
+--	(6,12),
+--	(7,12);
+
+
