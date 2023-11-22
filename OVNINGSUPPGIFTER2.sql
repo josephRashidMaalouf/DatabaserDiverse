ea322436a75060a0d53faa1e7feb08ab023f81c9
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2023-11-22 10:07:41 +0100

    Update OVNINGSUPPGIFTER2.sql

--- OVNINGSUPPGIFTER2.sql
+++ OVNINGSUPPGIFTER2.sql
@@ -2,26 +2,26 @@ USE everyloop;
 
 -- 1. Företagets totala produktkatalog består av 77 unika produkter. Om vi kollar bland våra ordrar, hur stor andel av dessa produkter har vi någon gång leverarat till London?
 
-SELECT *
-FROM company.products AS p
-JOIN company.order_details AS d ON p.Id = d.ProductId
-JOIN company.orders AS o ON o.Id = d.OrderId
-WHERE ShipCity = 'London'
-ORDER BY ProductName;
+--SELECT *
+--FROM company.products AS p
+--JOIN company.order_details AS d ON p.Id = d.ProductId
+--JOIN company.orders AS o ON o.Id = d.OrderId
+--WHERE ShipCity = 'London'
+--ORDER BY ProductName;
 
-SELECT COUNT(DISTINCT ProductName) AS [Unique products shipped], ShipCity 
-FROM company.products AS p
-JOIN company.order_details AS d ON p.Id = d.ProductId
-JOIN company.orders AS o ON o.Id = d.OrderId
-WHERE ShipCity = 'London'
-GROUP BY ShipCity;
+--SELECT COUNT(DISTINCT ProductName) AS [Unique products shipped], ShipCity 
+--FROM company.products AS p
+--JOIN company.order_details AS d ON p.Id = d.ProductId
+--JOIN company.orders AS o ON o.Id = d.OrderId
+--WHERE ShipCity = 'London'
+--GROUP BY ShipCity;
 
-SELECT (CONVERT(FLOAT, COUNT(DISTINCT ProductName) / 77.0) * 100) AS [Percentage], ShipCity 
-FROM company.products AS p
-JOIN company.order_details AS d ON p.Id = d.ProductId
-JOIN company.orders AS o ON o.Id = d.OrderId
-WHERE ShipCity = 'London'
-GROUP BY ShipCity;
+--SELECT (CONVERT(FLOAT, COUNT(DISTINCT ProductName) / 77.0) * 100) AS [Percentage], ShipCity 
+--FROM company.products AS p
+--JOIN company.order_details AS d ON p.Id = d.ProductId
+--JOIN company.orders AS o ON o.Id = d.OrderId
+--WHERE ShipCity = 'London'
+--GROUP BY ShipCity;
 
 ----## SVAR: 53,2% av företagets produkter har skickats till London.
 ----## Förklaring: 
@@ -36,13 +36,35 @@ GROUP BY ShipCity;
 
 -- 2. Till vilken stad har vi levererat flest unika produkter?
 
+--SELECT COUNT(DISTINCT ProductName) AS [Unique products shipped], ShipCity 
+--FROM company.products AS p
+--JOIN company.order_details AS d ON p.Id = d.ProductId
+--JOIN company.orders AS o ON o.Id = d.OrderId
+--GROUP BY ShipCity
+--ORDER BY [Unique products shipped] DESC;
 
-
+--## SVAR: Boise
+--## Förklaring:
+--Jag återanvände koden från förra uppgiften där jag räknade ut antalet unika produkter skickade till London,
+--men här tog jag bort vilkoret som filtrerar fram London, för att få en tabell över alla städer.
+--Därefter ordnade jag svaren utefter antalet unika produkter sålda för att lättare hitta staden som
+--företaget levererat flest unika produkter till. 
 
 
 -- 3. Av de produkter som inte längre finns I vårat sortiment, hur mycket har vi sålt för totalt till Tyskland?
 
 
+SELECT COUNT(ProductName) AS [Total products sold], COUNT(DISTINCT ProductName) AS [Unique products sold] , ShipCountry
+FROM company.products AS p
+JOIN company.order_details AS d ON p.Id = d.ProductId
+JOIN company.orders AS o ON o.Id = d.OrderId
+WHERE ShipCountry = 'Germany' AND Discontinued = 1
+GROUP BY ShipCountry;
+
+--## SVAR: Totalt 14 utgångna varor har sålts till Tyskland, varav 7 unika produkter.
+--## Förklaring: 
+--Samma tänk som i uppgifterna ovan, med skillnaden att jag i denna uppgift filtrerar efter tabellen Discontinued, och Land = Tyskland.
+--Jag var lite osäker på om uppgiften ville ha den totala mängden produkter sålda, eller endast unika varor, därför tog jag fram båda.
 
 
 -- 4. För vilken produktkategori har vi högst lagervärde?
