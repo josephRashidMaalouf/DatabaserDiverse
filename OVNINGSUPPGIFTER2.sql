c143eb3f77561152fd01f9f9be441dc26d224a82
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2023-11-22 16:37:16 +0100

    Update OVNINGSUPPGIFTER2.sql

--- OVNINGSUPPGIFTER2.sql
+++ OVNINGSUPPGIFTER2.sql
@@ -1,5 +1,5 @@
 USE everyloop;
-
+GO
 -- 1. Företagets totala produktkatalog består av 77 unika produkter. Om vi kollar bland våra ordrar, hur stor andel av dessa produkter har vi någon gång leverarat till London?
 
 --SELECT *
@@ -84,22 +84,69 @@ USE everyloop;
 
 -- 5. Från vilken leverantör har vi sålt flest produkter totalt under sommaren 2013?
 
-SELECT *
-FROM company.suppliers AS s
-JOIN company.products AS p ON p.SupplierId = s.Id
-JOIN company.order_details AS d ON d.ProductId = p.Id
-JOIN company.orders AS o ON d.OrderId = o.id;
+----SELECT *
+----FROM company.suppliers AS s
+----JOIN company.products AS p ON p.SupplierId = s.Id
+----JOIN company.order_details AS d ON d.ProductId = p.Id
+----JOIN company.orders AS o ON d.OrderId = o.id;
 
-SELECT CompanyName, ProductName, Quantity, d.Id, ShippedDate
-FROM company.suppliers AS s
-JOIN company.products AS p ON p.SupplierId = s.Id
-JOIN company.order_details AS d ON d.ProductId = p.Id
-JOIN company.orders AS o ON d.OrderId = o.id;
+----SELECT CompanyName, ProductName, Quantity, d.Id, ShippedDate
+----FROM company.suppliers AS s
+----JOIN company.products AS p ON p.SupplierId = s.Id
+----JOIN company.order_details AS d ON d.ProductId = p.Id
+----JOIN company.orders AS o ON d.OrderId = o.id;
 
-SELECT CompanyName, ProductName, Quantity, d.Id, ShippedDate
-FROM company.suppliers AS s
-JOIN company.products AS p ON p.SupplierId = s.Id
-JOIN company.order_details AS d ON d.ProductId = p.Id
-JOIN company.orders AS o ON d.OrderId = o.id
-WHERE ShippedDate LIKE ('2013-06%') OR ShippedDate LIKE ('2013-07%') OR ShippedDate LIKE ('2013-08%');
+----SELECT CompanyName, ProductName, Quantity, d.Id, ShippedDate
+----FROM company.suppliers AS s
+----JOIN company.products AS p ON p.SupplierId = s.Id
+----JOIN company.order_details AS d ON d.ProductId = p.Id
+----JOIN company.orders AS o ON d.OrderId = o.id
+----WHERE ShippedDate BETWEEN '2013-06' AND '2013-09'
+----ORDER BY CompanyName;
+
+--SELECT TOP 5 CompanyName, SUM(Quantity) AS [Products sold summer 2013]
+--FROM company.suppliers AS s
+--	JOIN company.products AS p ON p.SupplierId = s.Id
+--	JOIN company.order_details AS d ON d.ProductId = p.Id
+--	JOIN company.orders AS o ON d.OrderId = o.id
+--WHERE ShippedDate BETWEEN '2013-06' AND '2013-09'
+--GROUP BY CompanyName
+--ORDER BY [Products sold summer 2013] DESC;
+
+--## Svar: Plutzer LebensmittelgroBmärkte AG sålde 478 produkter sommaren 2013 och var det bäst säljande företaget.
+--## Förklaring:
+--Jag räknade med juni, juli och augusti som sommarmånader, och använde ShippedDate när som datum för när produkten såldes.
+--För att få fram detta svar slog jag ihop tabellerna supliers, products, order_details och orders.
+--Jag filtrerade ut sommarsäljen med hjälp av operatorn BETWEEN. '2013-06' AND '2013-09' innebär att alla datum från första juni till sista augusti filtreras fram.
+--Jag räknade ihop antalet sälj genom operatorn SUM på kolumnen Quantity, och grupperade ihop på companyname för att få summan på varje företags försäljningar.
+--Detta gav mig svaret: Plutzer LebensmittelgroBmärkte AG sålde 478 produkter sommaren 2013 och var det bäst säljande företaget. Den sista queryn var alltså den
+--som ledde mig till rätt svar.
+
+--########## DEL 2 ##########
+
+
+--declare @playlist varchar(max) = 'Heavy Metal Classic';
+--GO
+
+CREATE OR ALTER VIEW [Music]
+AS
+SELECT 
+g.Name AS [Genre],
+ar.Name AS [Artist],
+a.Title AS [Album],
+t.Name AS [Track],
+FORMAT(DATEADD(SECOND, t.Milliseconds / 1000, 0), 'mm:ss') AS [Length],
+CONVERT(NVARCHAR(10), ROUND(CONVERT(FLOAT, t.Bytes / (1024 * 1024.0)), 1)) + ' ' + 'MiB' AS [Size],
+t.Composer AS [Composer]
+FROM music.playlists p
+JOIN music.playlist_track pjt ON p.PlaylistId = pjt.PlaylistId
+JOIN music.tracks t ON pjt.TrackId = t.TrackId
+JOIN music.albums a ON t.AlbumId = a.AlbumId
+JOIN music.genres g ON g.GenreId = t.GenreId
+JOIN music.artists ar ON ar.ArtistId = a.ArtistId;
+GO
+
+
+
+
 
